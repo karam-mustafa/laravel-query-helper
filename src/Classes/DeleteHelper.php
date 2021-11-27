a4a5ff1069fb47713da42a58addcf9f1cfc51c5e
karam  mustafa <karam2mustafa@gmail.com>
2021-11-28 00:34:10 +0200

    add delete all tables feature

--- src/Classes/DeleteHelper.php
+++ src/Classes/DeleteHelper.php
@@ -4,7 +4,30 @@
 namespace KMLaravel\QueryHelper\Classes;
 
 
-class DeleteHelper
+/**
+ * Class DeleteHelper
+ *
+ * @author karam mustafa
+ * @package KMLaravel\QueryHelper\Classes
+ */
+class DeleteHelper extends BaseHelper
 {
 
+    /**
+     * drope multiple tables by their names in the database
+     *
+     * @return DeleteHelper
+     * @author karam mustafa
+     */
+    public function dropMultiTables()
+    {
+        $tables = $this->tables;
+        $query = "";
+        foreach ($tables as $index => $table) {
+            $query .= $index == 0 ? "`$table`" : ",`$table`";
+        }
+        $this->setQuery(sprintf("DROP TABLE %s;", $query));
+
+        return $this;
+    }
 }
